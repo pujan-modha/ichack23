9d82d269b14450173f69cffe22e41b11793fb515
Pujan Modha <pujanmodha19@gmail.com>
2023-09-16 19:52:21 +0530

    some more minor design fixes v6.9.1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                         It is a 48-hour hackathon where participants are given a problem statement on the spot and have to create a coding solution to it.
                     </p>
                     <div className="flex flex-col mt-8 lg:mt-0 space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                        <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-medium rounded bg-pracula text-dracxt">Register Now</a>
+                        <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-medium border-2 border-pracula hover:text-pracula rounded-lg hover:bg-opacity-0 bg-pracula text-dracxt">REGISTER NOW</a>
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center p-6 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 overflow-hidden" data-aos="fade-up">
