264b446424a82423ffc94e25c5744eae87261ed9
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-17 01:49:59 +0530

    FORM

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -102,18 +102,18 @@ const Contact = () => {
                         <div className="mx-auto max-w-screen-md">
                             {/*<h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>*/}
                             {/*<p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>*/}
-                            <form action="https://formspree.io/f/xrgwkwlwnpm" className="space-y-8 " method="POST">
+                            <form action="https://formspree.io/f/xrgwkwlw" className="space-y-8 " method="POST">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-pracula">Your email</label>
-                                    <input type="email" id="email" className="shadow-sm bg-pracula border border-pracula text-dracxt text-sm rounded-lg focus:ring-pracula focus:border-pracula block w-full p-2.5 bg-opacity-5 backdrop-blur-3xl" placeholder="name@example.com" required/>
+                                    <input name="Email" type="email" id="email" className=" shadow-sm bg-pracula border border-pracula text-dracxt text-sm rounded-lg focus:ring-pracula focus:border-pracula block w-full p-2.5 bg-opacity-5 backdrop-blur-3xl" placeholder="name@example.com" required/>
                                 </div>
                                 <div>
                                     <label htmlFor="subject" className="block mb-2 text-sm font-medium text-pracula">Subject</label>
-                                    <input type="text" id="subject" className="shadow-sm bg-pracula border border-pracula text-dracxt text-sm rounded-lg focus:ring-pracula focus:border-pracula block w-full p-2.5 bg-opacity-5 backdrop-blur-3xl" placeholder="Let us know how we can help you" required/>
+                                    <input name="Subject" type="text" id="subject" className="shadow-sm bg-pracula border border-pracula text-dracxt text-sm rounded-lg focus:ring-pracula focus:border-pracula block w-full p-2.5 bg-opacity-5 backdrop-blur-3xl" placeholder="Let us know how we can help you" required/>
                                 </div>
                                 <div className="sm:col-span-2">
                                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-pracula">Your message</label>
-                                    <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-dracxt bg-pracula rounded-lg shadow-sm border border-pracula backdrop-blur-3xl bg-opacity-5 focus:ring-pracula focus:border-pracula" placeholder="Leave a comment..."></textarea>
+                                    <textarea name="Message" id="message" rows="6" className="block p-2.5 w-full text-sm text-dracxt bg-pracula rounded-lg shadow-sm border border-pracula backdrop-blur-3xl bg-opacity-5 focus:ring-pracula focus:border-pracula" placeholder="Leave a comment..."></textarea>
                                 </div>
                                 <button type="submit" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center hover:text-dracxt rounded-lg hover:bg-pracula hover:border-2 text-pracula hover:border-dracula bg-dracula border-2 border-pracula">Send Message</button>
                             </form>
