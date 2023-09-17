f64c550a4b9ac0930f041a82e1e6d03e9e551fe8
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-17 16:20:15 +0530

    Changed About data

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -11,7 +11,12 @@ const AboutCard = () => {
                     <img className="inline max-h-full max-w-full align-middle justify-between object-cover w-full rounded-t-lg h-full md:h-full md:w-96 md:rounded-none md:rounded-l-lg p-[1%]" src={ichack} alt=""/>
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h1 className="text-2xl lg:text-4xl font-bold tracking-tight text-pracula mb-[2vh]">IC HACK 2023</h1>
-                        <p className="mb-[3vh] font-normal text-dracxt lg:text-2xl text-justify">IEEE India Council Hack’23 is the student-run Hackathon, with its prime motive to provide a recognised platform to every tech enthusiast. Our 48-hour event will assist the participants in creating a coding solution to the problem statement, which is given on-spot to the participants. It aims to be an all-inclusive event that allows participants to express their creativity and eliminate the social obstacles surrounding programming. They need to show their managerial skills, teamwork, and the witty tactics they will implement to solve the problem. The Hackathon will offer a competitive environment focused on increasing diversity in the field of computer science. We want to inspire students from around India and beyond to enter the field of computer science, where their basic ideas may become the industry-level solution in future. We are trying to imbibe industry exposure to the participants, which will help them in the long run also want to spread the idea that programming is a useful skill that anyone can enjoy.</p>
+                        <p className="mb-[3vh] font-normal text-dracxt lg:text-2xl text-justify">IEEE IC HACK ‘23 is a student-run hackathon brought to you by IEEE SB MUJ and IEEE India Council.
+
+                            It is a 36-hour event where participants from all over India come together to build innovative solutions to real-world problems. The hackathon is open to all students, regardless of their skill level or background.
+
+                            IEEE IC HACK ‘23 is a great opportunity for students to learn new skills, network with other tech enthusiasts, and build innovative solutions to real-world problems.
+                        </p>
                         <div className='pb-[1%] xl:max-w-[12.5vw]'>
                             <a href="/" className='bg-pracula px-3 py-2 border-2 border-pracula rounded-lg hover:bg-dracula hover:text-pracula'>
                                 <button type='button'>
--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -14,25 +14,33 @@ const AboutData = [
     {
         aboutName: "IEEE India Council",
         aboutImage: indiacouncillogo,
-        aboutAbout: "IEEE India Council is the umbrella organization that coordinates IEEE activities in India. India Council comes under IEEE Region 10, i.e., Asia Pacific region of IEEE, and has over 50,000 members spread across 1000 student branches. IEEE India Council was established on 20th May 1976 and is one of the five councils in Asia Pacific.",
+        aboutAbout: "IEEE India Council is the umbrella organization which coordinates IEEE activities in India.\n" +
+            "\n" +
+            "IEEE India Council strives to aid in the advancement of technology in India, and provides a recognised platform for the tech enthusiasts to grow together in this complex field of science.\n",
         aboutUrl: "https://ieeeindiacouncil.org/"
     },
     {
         aboutName: "IEEE",
         aboutImage: placeHolder,
-        aboutAbout: "IEEE (Institute of Electrical and Electronics Engineers) is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity. IEEE aims to recognize the contributions of technical communities and technical professionals in improving global conditions. IEEE and its members inspire a global community through highly cited publications, conferences, technology standards, and professional and educational activities.",
+        aboutAbout: "IEEE is the world’s largest technical professional organization dedicated to advancing technology for the benefit of humanity.\n" +
+            "\n" +
+            "IEEE and its members inspire a global community to innovate for a better tomorrow through its more than 400,000 members in over 160 countries, and its highly cited publications, conferences, technology standards, and professional and educational activities.\n",
         aboutUrl: "https://ieee.org/"
     },
     {
         aboutName: "Manipal University Jaipur",
         aboutImage: mujlogo,
-        aboutAbout: "Manipal University Jaipur (MUJ) was launched in 2011 on an invitation from the Government of Rajasthan, as a self-financed State University. MUJ has redefined academic excellence in the region, with the Manipal way of learning; one that inspires students of all disciplines to learn and innovate through hands on practical experience.",
+        aboutAbout: "Manipal University Jaipur was launched in 2011 on an invitation from the Government of Rajasthan, as a self-financed State University.\n" +
+            "\n" +
+            "MUJ has redefined academic excellence in the region, with the Manipal way of learning. One that inspires students of all disciplines to learn and innovate through hands-on practical experience.\n",
         aboutUrl: "https://jaipur.manipal.edu/"
     },
     {
         aboutName: "IEEE SB MUJ",
         aboutImage: sblogo,
-        aboutAbout: "IEEE Student Branch, Manipal University Jaipur, strives to create and spread awareness of the various technologies that surround us, promote research among students, and recognize technological excellence. We have grown into a multi-faceted chapter, embodying IEEE Computer Society MUJ, IEEE Antennas and Propagation Society MUJ, IEEE Microwave Theory and Techniques Society MUJ and IEEE Women in Engineering Affinity Group MUJ. We share a common goal of empowering young engineers to enhance their skills and set up milestones. IEEE is the trusted “voice” for engineering, computing, and technology information around the globe.",
+        aboutAbout: "IEEE Student Branch, Manipal University Jaipur is a dedicated group of students who strive to build and support a platform for like-minded tech enthusiasts to network and collaborate.\n" +
+            "\n" +
+            "In our pursuit for quality and practical knowledge, we are guided by a group of dedicated faculty members who are relentless in their efforts to hone our potential and mold us into the best engineers we could possibly become.\n",
         aboutUrl: "https://ieeemuj.com/"
     },
 ];
