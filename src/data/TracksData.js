78e8ba965a6a03f68e599f69b8487346f1f8a26e
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-17 16:25:26 +0530

    Changed Track data

--- src/data/TracksData.js
+++ src/data/TracksData.js
@@ -5,27 +5,27 @@ const TracksData = [
     {
         tracksImage: placeHolder,
         tracksName: "CLIMATE CHANGE AND SUSTAINABILITY",
-        tracksAbout: "Climate refers to the kind of weather that's typically expected in a region. This includes describing the range of conditions that are possible. Climate change is a long-term shift in the average weather conditions of a region, such as its typical temperature, rainfall, and windiness. Sustainability is concerned with protecting the planet, halting climate change and promoting social development, without endangering life on Earth or leaving anyone behind. This concept seeks to cover our present needs without compromising resources for future generations."
+        tracksAbout: "Tackle some of the world's most pressing environmental challenges in the Climate Change and Sustainability track. Develop innovative solutions that promote sustainable practices, reduce carbon footprints, conserve natural resources, and address climate change issues. Join us in building a greener future for our planet."
     },
     {
         tracksImage: placeHolder,
         tracksName: "CS AND SIGNAL PROCESSING SOCIETY",
-        tracksAbout: "The IEEE Computer Society is the premier source for information, inspiration, and collaboration in computer science and engineering. Connecting members worldwide, the Computer Society empowers the people who advance technology by delivering tools for individuals at all stages of their professional careers. Our trusted resources include international conferences, peer-reviewed publications, a robust digital library, globally recognized standards, and continuous learning opportunities.",
+        tracksAbout: "Dive into the world of Computer Science and Signal Processing in this track. Explore cutting-edge technologies, algorithms, and methods to solve complex problems related to digital signal processing, data analysis, and more. Join hands with experts and enthusiasts to advance the field of signal processing and computer science.",
     },
     {
         tracksImage: placeHolder,
-        tracksName: "GRSS",
-        tracksAbout: "The IEEE Computer Society is the premier source for information, inspiration, and collaboration in computer science and engineering. Connecting members worldwide, the Computer Society empowers the people who advance technology by delivering tools for individuals at all stages of their professional careers. Our trusted resources include international conferences, peer-reviewed publications, a robust digital library, globally recognized standards, and continuous learning opportunities.",
+        tracksName: "GRSS ",
+        tracksAbout: "Join the Geoscience and Remote Sensing Society track to work on projects related to Earth observation, remote sensing technologies, and geospatial data analysis. Make a difference by leveraging satellite data, GIS, and advanced geospatial tools to address real-world challenges in environmental monitoring, agriculture, disaster management, and more.",
     },
     {
         tracksImage: placeHolder,
         tracksName: "AESS AND MTTAPS",
-        tracksAbout: "The IEEE Computer Society is the premier source for information, inspiration, and collaboration in computer science and engineering. Connecting members worldwide, the Computer Society empowers the people who advance technology by delivering tools for individuals at all stages of their professional careers. Our trusted resources include international conferences, peer-reviewed publications, a robust digital library, globally recognized standards, and continuous learning opportunities.",
+        tracksAbout: "Embark on a journey into the realms of aerospace and electronic systems with the AESS and MTT-APS track. Develop groundbreaking solutions in fields like radar technology, antennas, aerospace systems, and wireless communication. Collaborate with experts and fellow enthusiasts to push the boundaries of aerospace and electronics.",
     },
     {
         tracksImage: placeHolder,
-        tracksName: "OPEN AUTOMATION",
-        tracksAbout: "The IEEE Computer Society is the premier source for information, inspiration, and collaboration in computer science and engineering. Connecting members worldwide, the Computer Society empowers the people who advance technology by delivering tools for individuals at all stages of their professional careers. Our trusted resources include international conferences, peer-reviewed publications, a robust digital library, globally recognized standards, and continuous learning opportunities.",
+        tracksName: "OPEN INNOVATION",
+        tracksAbout: "Open Innovation is the track for those who think beyond boundaries and are ready to disrupt industries. This track encourages participants to bring fresh ideas and creativity to the forefront. Solve problems, create new opportunities, and drive innovation across various domains. Join us to shape the future through open collaboration.\n",
     },
 ];
 
