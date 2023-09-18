beac1beebe639c97bca43426642498a22212f2c5
Pujan Modha <pujanmodha19@gmail.com>
2023-09-19 03:37:48 +0530

    team page changes

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -17,10 +17,10 @@ const TeamCard = () => {
                                 alt={teams.personName}
                             />
                             <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-pracula bg-opacity-100 opacity-0 hover:opacity-100">
-                                <p className="mb-4 text-xl font-bold text-dracula">
+                                <p className="mb-4 text-5xl lg:text-3xl font-bold text-dracula">
                                     {teams.personName}
                                 </p>
-                                <p className="mb-4 text-sm text-dracula">{teams.personPosition} @ {teams.personOrganization}</p>
+                                <p className="mb-4 text-lg lg:text-base text-dracula">{teams.personPosition} @ {teams.personOrganization}</p>
                                 {/*<p className="mb-4 text-sm text-dracula">*/}
                                 {/*    {teams.personOrganization}*/}
                                 {/*</p>*/}
@@ -29,7 +29,7 @@ const TeamCard = () => {
                                         href={teams.personLinkedIn}
                                         className="text-dracula"
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512" fill="#282a36"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1.75em" viewBox="0 0 448 512" fill="#282a36"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
                                     </a>
                                 </div>
                             </div>
