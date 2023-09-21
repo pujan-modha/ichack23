ae8217ef438592f2195dc509e355e19587996c2b
Pujan Modha <pujanmodha19@gmail.com>
2023-09-22 01:20:57 +0530

    new tracks style on home v2

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,4 +1,9 @@
 import CCS from "../assets/tracks/ccs.png";
+import CSSPS from "../assets/tracks/cssps.png";
+import GRSS from "../assets/tracks/grss.png";
+import AESSMTTAPS from "../assets/tracks/aessmttaps.png";
+import OI from "../assets/tracks/openinnovation.png";
+
 
 const HomeCard = () => {
     return(
@@ -10,9 +15,9 @@ const HomeCard = () => {
                         <div>
                             <div className='items-center justify-around justify-items-center'>
                                 <div className="w-auto justify-center items-center">
-                                    <img className="rounded-t-lg m-auto pt-5" src={CCS} alt="" />
+                                    <img className="rounded-t-lg m-auto pt-5 w-[50%]" src={CCS} alt="" />
                                     <div className="grid p-5 align-middle place-items-center items-center justify-items-center">
-                                        <h5 className="mb-2 lg:text-4xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
+                                        <h5 className="mb-2 lg:text-3xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
                                             CLIMATE CHANGE AND SUSTAINABILITY
                                         </h5>
                                         <a rel="noopener noreferrer" href="/"
@@ -29,9 +34,9 @@ const HomeCard = () => {
                         <div>
                             <div className='items-center justify-around justify-items-center'>
                                 <div className="w-auto justify-center items-center">
-                                    <img className="rounded-t-lg m-auto pt-5" src={CCS} alt="" />
+                                    <img className="rounded-t-lg m-auto pt-5 w-[50%]" src={CSSPS} alt="" />
                                     <div className="grid p-5 align-middle place-items-center items-center justify-items-center">
-                                        <h5 className="mb-2 lg:text-4xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
+                                        <h5 className="mb-2 lg:text-3xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
                                             CS AND SIGNAL PROCESSING SOCIETY
                                         </h5>
                                         <a rel="noopener noreferrer" href="/"
@@ -48,9 +53,9 @@ const HomeCard = () => {
                         <div>
                             <div className='items-center justify-around justify-items-center'>
                                 <div className="w-auto justify-center items-center">
-                                    <img className="rounded-t-lg m-auto pt-5" src={CCS} alt="" />
+                                    <img className="rounded-t-lg m-auto pt-5 w-[50%]" src={GRSS} alt="" />
                                     <div className="grid p-5 align-middle place-items-center items-center justify-items-center">
-                                        <h5 className="mb-2 lg:text-4xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
+                                        <h5 className="mb-2 lg:text-3xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
                                             GEOSCIENCE AND REMOTE SENSING SOCIETY
                                         </h5>
                                         <a rel="noopener noreferrer" href="/"
@@ -67,9 +72,9 @@ const HomeCard = () => {
                         <div>
                             <div className='items-center justify-around justify-items-center'>
                                 <div className="w-auto justify-center items-center">
-                                    <img className="rounded-t-lg m-auto pt-5" src={CCS} alt="" />
+                                    <img className="rounded-t-lg m-auto pt-5 w-[50%]" src={AESSMTTAPS} alt="" />
                                     <div className="grid p-5 align-middle place-items-center items-center justify-items-center">
-                                        <h5 className="mb-2 lg:text-4xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
+                                        <h5 className="mb-2 lg:text-3xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
                                             AESS AND MTTAPS
                                         </h5>
                                         <a rel="noopener noreferrer" href="/"
@@ -86,10 +91,11 @@ const HomeCard = () => {
                         <div>
                             <div className='items-center justify-around justify-items-center'>
                                 <div className="w-auto justify-center items-center">
-                                    <img className="rounded-t-lg m-auto pt-5" src={CCS} alt="" />
+                                    <img className="rounded-t-lg m-auto pt-5 w-[50%]" src={OI} alt="" />
                                     <div className="grid p-5 align-middle place-items-center items-center justify-items-center">
-                                        <h5 className="mb-2 lg:text-4xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
-                                            OPEN INNOVATION                                        </h5>
+                                        <h5 className="mb-2 lg:text-3xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold tracking-tight">
+                                            OPEN INNOVATION
+                                        </h5>
                                         <a rel="noopener noreferrer" href="/"
                                            className="px-5 py-2 text-lg border-2 border-pracula hover:text-pracula rounded-lg hover:bg-opacity-0 bg-pracula text-dracxt">
                                             KNOW MORE
