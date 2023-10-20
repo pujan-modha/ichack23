7b348159bae56f9341a5218e503617f938994889
Pujan Modha <pujanmodha19@gmail.com>
2023-10-20 18:14:55 +0530

    added venue and prize-pool modifications

--- src/components/SponsorsHome.jsx
+++ src/components/SponsorsHome.jsx
@@ -3,6 +3,7 @@ import CT from "../assets/sponsors/CT.png";
 import ECELL from "../assets/sponsors/ECELL.png";
 import PC from "../assets/sponsors/PC.png";
 import TV from "../assets/sponsors/TV.png";
+import MUJ from "../assets/mujlogo.png";
 import React from "react";
 
 
@@ -90,6 +91,24 @@ const SponsorsHome = () => {
                                             Sponsor
                                         </h5>
                                         <a rel="noopener noreferrer" href="/sponsors#ECELL"
+                                           className="px-5 py-2 mt-1 text-lg border-2 border-pracula hover:text-pracula rounded-lg hover:bg-opacity-0 bg-pracula text-dracxt">
+                                            KNOW MORE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='grid grid-cols-1 gap-16 justify-center justify-items-center align-middle lg:grid-cols-3 mx-auto mt-16'>
+                        <div className='lg:col-start-2'>
+                            <div className='items-center justify-around justify-items-center'>
+                                <div className="w-auto justify-center items-center">
+                                    <img className="rounded-t-lg m-auto pt-5 w-auto h-64" src={MUJ} alt="" />
+                                    <div className="grid p-5 align-middle place-items-center items-center justify-items-center">
+                                        <h5 className="mb-2 lg:text-3xl md:text-3xl text-2xl text-center text-pracula font-bold lg:font-semibold">
+                                            Venue
+                                        </h5>
+                                        <a rel="noopener noreferrer" href="https://jaipur.manipal.edu/"
                                            className="px-5 py-2 mt-1 text-lg border-2 border-pracula hover:text-pracula rounded-lg hover:bg-opacity-0 bg-pracula text-dracxt">
                                             KNOW MORE
                                         </a>
